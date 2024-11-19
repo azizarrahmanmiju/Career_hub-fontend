2e14e4d900a6b1e1f5f42af393a8e742579196d1
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-19 17:24:47 +0600

    HomeScreen Cmplete

--- src/compoment/Titanddesc/TitandDesc.jsx
+++ src/compoment/Titanddesc/TitandDesc.jsx
@@ -4,7 +4,7 @@ const TitandDesc = ({ title, description }) => {
     return (
         <div className="container justify-center text-center">
             <h2 className='font-bold text-5xl text-black  mt-[100px]'>{title}</h2>
-            <p className='text-sm text-center text-gray-400'>{description}</p>
+            <p className='text-sm text-center text-gray-400 mt-5'>{description}</p>
         </div>
     );
 };
